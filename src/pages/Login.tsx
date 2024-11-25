76c4edc67711c846aad6e43f3d5d33ceeb48884f
FJavier <fjavierme818@gmail.com>
2024-11-24 19:02:35 -0600

    chore: initializing project with React, TypeScript, React Router and TailwindCSS

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+// src/pages/Home.tsx
+import React from 'react';
+
+export const Login: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-4">Bienvenido a la app de Control de Acceso</h1>
+    </div>
+  );
+};
+
